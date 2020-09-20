cd15c85b7935ea44888443b6c1ba82b85e78688b
Artyom Lonsky <artyom.lonsky@gmail.com>
2020-09-20 12:24:14 +0300

    Initial; Base scene setup and player jump;

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 400f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
+
+    private const float GROUNDED_RADIUS = .2f;
+    private bool grounded;
+    private bool jump = false;
+
+    private Rigidbody2D rigidbody2D;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, GROUNDED_RADIUS, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+            }
+        }
+
+        // If the player should jump...
+        if (grounded && jump)
+        {
+            // Add a vertical force to the player.
+            grounded = false;
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        }
+        jump = false;
+    }
+}
